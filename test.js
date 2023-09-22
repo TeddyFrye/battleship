dc77581f4a917b496e04dbe840e9aaf293613618
TeddyFrye <tfrye1227@gmail.com>
2023-09-22 10:48:07 -0500

    Adds base files and working test file

---
+++ test.js
@@ -0,0 +1,6 @@
+//test for tests to work
+const myFunction = require("./script.js"); // Adjust the import to your function or module
+
+test("it should work", () => {
+  expect(myFunction()).toBe(true); // Adjust the expectation to your actual function
+});
