1d78d7b9c267ea0e05342f595ef6fa7e532cd2fd
TeddyFrye <tfrye1227@gmail.com>
2023-09-22 12:26:20 -0500

    Adds ship tests and factory

---
+++ ship.js
@@ -0,0 +1,18 @@
+// Ship Factory Function
+function Ship(length) {
+  let hitCount = 0;
+
+  function hit() {
+    if (hitCount < length) {
+      hitCount++;
+    }
+  }
+
+  function isSunk() {
+    return hitCount >= length;
+  }
+
+  return { length, hit, isSunk };
+}
+
+module.exports = Ship;
