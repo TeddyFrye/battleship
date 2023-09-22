417ac862190ca42d7d79911d9f94299bccc9329c
TeddyFrye <tfrye1227@gmail.com>
2023-09-22 12:26:20 -0500

    Adds ship tests and factory

--- test.js
+++ test.js
@@ -4,3 +4,30 @@ const myFunction = require("./script.js"); // Adjust the import to your function
 test("it should work", () => {
   expect(myFunction()).toBe(true); // Adjust the expectation to your actual function
 });
+
+// Tests for Ship
+const Ship = require("./ship");
+
+test("should initialize with correct length", () => {
+  const ship = Ship(3);
+  expect(ship.length).toBe(3);
+});
+
+test("should correctly report when it is not sunk", () => {
+  const ship = Ship(3);
+  expect(ship.isSunk()).toBe(false);
+});
+
+test("should correctly report when it is sunk", () => {
+  const ship = Ship(3);
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("should be able to get hit", () => {
+  const ship = Ship(3);
+  ship.hit();
+  expect(ship.isSunk()).toBe(false);
+});
