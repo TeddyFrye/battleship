7d8a4eabc3a5084c1e5ca4b7355251bd9a52710f
TeddyFrye <tfrye1227@gmail.com>
2023-09-29 18:30:44 -0500

    renames dirs and scripts

--- script/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: "./script.js", // Your entry point file
-  output: {
-    filename: "bundle.js", // The name of the bundled file
-    path: path.resolve(__dirname, "dist"), // The directory where the bundled file should be saved
-  },
-};
