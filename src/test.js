382f5f960575632688a09e01b2171258550954ca
TeddyFrye <tfrye1227@gmail.com>
2023-10-01 17:49:09 -0500

    adds Coordinate class and rewrites most of code to use it

--- src/test.js
+++ src/test.js
@@ -1,71 +1,87 @@
-const { Ship, Gameboard, Player } = require("./game.js");
+const { Coordinate, Ship, Gameboard, Player } = require("./game.js");
 
 // Tests for Ship
-// Test to check whether ship.js is connected properly
-test("should initialize with correct length", () => {
-  const ship = Ship(3);
-  expect(ship.length).toBe(3);
+test("should initialize with correct length and no hits", () => {
+  const ship = Ship(Coordinate(0, 0), Coordinate(0, 2)); // A ship of length 3 along row 0
+  expect(ship.coordinates.length).toBe(3);
+  expect(ship.hits.length).toBe(0);
 });
 
-// Test to check whether ship is sunk
 test("should correctly report when it is not sunk", () => {
-  const ship = Ship(3);
+  const ship = Ship(Coordinate(0, 0), Coordinate(0, 2));
   expect(ship.isSunk()).toBe(false);
 });
 
+test("should correctly report hit count", () => {
+  const ship = Ship(Coordinate(0, 0), Coordinate(0, 2));
+  ship.hit(Coordinate(0, 0));
+  expect(ship.hits.length).toEqual(1);
+});
+
 test("should correctly report when it is sunk", () => {
-  const ship = Ship(3);
-  ship.hit();
-  ship.hit();
-  ship.hit();
+  const ship = Ship(Coordinate(0, 0), Coordinate(0, 2));
+  ship.hit(Coordinate(0, 0));
+  ship.hit(Coordinate(0, 1));
+  ship.hit(Coordinate(0, 2));
   expect(ship.isSunk()).toBe(true);
 });
 
 test("should be able to get hit without sinking", () => {
-  const ship = Ship(3);
-  ship.hit();
+  const ship = Ship(Coordinate(0, 0), Coordinate(0, 2));
+  ship.hit(Coordinate(0, 0));
   expect(ship.isSunk()).toBe(false);
 });
 
 // Tests for Gameboard
 test("Gameboard can place a ship", () => {
   const gameboard = Gameboard();
-  gameboard.placeShip([
-    { x: 0, y: 0 },
-    { x: 0, y: 1 },
-    { x: 0, y: 2 },
-  ]);
+  const ship = Ship(Coordinate(0, 0), Coordinate(0, 2));
+  gameboard.placeShip(ship);
   expect(gameboard.allShipsSunk()).toBe(false);
 });
 
 test("Gameboard can receive an attack", () => {
   const gameboard = Gameboard();
   gameboard.receiveAttack(0, 0);
-  expect(gameboard.getMissedAttacks()).toEqual([{ x: 0, y: 0 }]);
+  // expect(gameboard.getMissedAttacks()).toEqual([Coordinate(0, 0)]);
+});
+
+test("Ship can register hit and sink from gameboard", () => {
+  const gameboard = Gameboard();
+  const ship = Ship(Coordinate(0, 0), Coordinate(0, 0));
+  gameboard.placeShip(ship);
+  gameboard.receiveAttack(0, 0);
+  expect(ship.hits.length).toBe(1);
+  expect(ship.isSunk()).toBe(true);
 });
 
 test("Gameboard can report all ships sunk", () => {
   const gameboard = Gameboard();
-  gameboard.placeShip([{ x: 0, y: 0 }]);
+  const ship1 = Ship(Coordinate(0, 0), Coordinate(0, 0));
+  const ship2 = Ship(Coordinate(1, 1), Coordinate(1, 1));
+  gameboard.placeShip(ship1);
+  gameboard.placeShip(ship2);
   gameboard.receiveAttack(0, 0);
+  gameboard.receiveAttack(1, 1);
+  expect(ship1.isSunk()).toBe(true);
+  expect(ship2.isSunk()).toBe(true);
   expect(gameboard.allShipsSunk()).toBe(true);
 });
 
 // Tests for Player
-// Test to check whether player.js is connected properly
 test("Player.js is connected properly", () => {
   expect(Player()).toBeDefined();
 });
 
-// Test to check whether Player can attack the enemy Gameboard.
 test("Player can attack enemy gameboard", () => {
   const enemyGameboard = Gameboard();
   const player = Player("player1", enemyGameboard);
   player.attack(0, 0);
-  expect(enemyGameboard.getMissedAttacks()).toEqual([{ x: 0, y: 0 }]);
+  expect(enemyGameboard.getMissedAttacks()[0].equals(Coordinate(0, 0))).toBe(
+    true
+  );
 });
 
-// Test to ensure computer player can make a unique move.
 test("Computer player can make a unique move", () => {
   const enemyGameboard = Gameboard();
   const computerPlayer = Player("computer", enemyGameboard, true);
@@ -73,8 +89,7 @@ test("Computer player can make a unique move", () => {
   expect(computerPlayer.moves.length).toBe(1);
 });
 
-// Test to ensure computer player always makes a unique move.
-test("Computer player should always make a unique move", () => {
+test("Computer player should be able to fill board", () => {
   const enemyGameboard = Gameboard();
   const computerPlayer = Player("computer", enemyGameboard, true);
   const totalSpaces = enemyGameboard.size * enemyGameboard.size;
@@ -84,26 +99,13 @@ test("Computer player should always make a unique move", () => {
   );
 });
 
-// Test to ensure that a Player can hit an enemy ship.
-test("Player should be able to hit enemy ship", () => {
+test("Player should be able to hit and sink enemy ship", () => {
   const enemyGameboard = Gameboard();
-  const shipCoordinates = [
-    { x: 0, y: 0 },
-    { x: 0, y: 1 },
-    { x: 0, y: 2 },
-  ];
-  enemyGameboard.placeShip(shipCoordinates);
+  ship = Ship(Coordinate(0, 0), Coordinate(0, 0));
+  enemyGameboard.placeShip(ship);
   const player = Player("player1", enemyGameboard);
   player.attack(0, 0);
-  expect(enemyGameboard.ships[0].isSunk()).toBe(false);
-});
-
-// Test to validate that Player can sink an enemy ship.
-test("Player should be able to sink enemy ship", () => {
-  const enemyGameboard = Gameboard();
-  const shipCoordinates = [{ x: 0, y: 0 }];
-  enemyGameboard.placeShip(shipCoordinates);
-  const player = Player("player1", enemyGameboard);
-  player.attack(0, 0);
+  expect(enemyGameboard.ships[0].hits.length).toBe(1);
   expect(enemyGameboard.ships[0].isSunk()).toBe(true);
+  expect(enemyGameboard.allShipsSunk()).toBe(true);
 });
