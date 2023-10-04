83f3a5537d19f9db0d62aef5d91740f1e3463572
TeddyFrye <tfrye1227@gmail.com>
2023-10-04 18:21:15 -0500

    Adds game function and tracks winner status

--- src/script.js
+++ src/script.js
@@ -1,17 +1,6 @@
-import {
-  gameStart,
-  gameStep,
-  playerBoard,
-  computerBoard,
-  player,
-  computer,
-} from "./game";
+import { Game } from "./game";
 
-window.gameStart = gameStart;
-window.gameStep = gameStep;
-window.playerBoard = playerBoard;
-window.computerBoard = computerBoard;
-window.player = player;
-window.computer = computer;
+const game = Game();
+window.game = game;
 
-window.gameStart();
+window.game.placeDefault();
